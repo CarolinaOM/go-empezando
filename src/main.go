c5341f70a3121977d9b58f77789d7ccb4d7556bb
CarolinaOM <caro13081991@gmail.com>
2022-01-25 12:29:45 +0000

    Agregando contenido al main sobre if

--- src/main.go
+++ src/main.go
@@ -22,4 +22,26 @@ func main() {
 
 	fmt.Printf("Nombre: %s Edad: %d \nvalor de Pi: %f", nombre, edad, pi)
 	fmt.Println()
+
+	fmt.Printf("mi nombre es: %s", nombre)
+	fmt.Println()
+
+	// Metodo if
+
+	if false {
+		fmt.Println("Se cumple la condicion")
+	} else {
+		fmt.Println("No se cumple la condicion")
+	}
+
+	var n int
+	fmt.Print("Ingrese el numero: ")
+	fmt.Scanln(&n)
+	if n == 0 {
+		fmt.Println(n, "Es neutro")
+	} else if n%2 == 0 {
+		fmt.Println(n, "Es par")
+	} else {
+		fmt.Println(n, "Es impar")
+	}
 }
