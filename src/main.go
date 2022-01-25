9ec4b1eb31ae2deea6ac2cec0ae7be7ac8fd574f
CarolinaOM <caro13081991@gmail.com>
2022-01-25 00:07:15 +0000

    Agregando contenido al main

--- src/main.go
+++ src/main.go
@@ -5,4 +5,21 @@ import "fmt"
 func main() {
 	greeting := "Hello There"
 	fmt.Println(greeting)
+
+	var (
+		nombre string
+		edad   int
+		pi     float64
+	)
+	fmt.Print("Ingrese su nombre: ")
+	fmt.Scanln(&nombre)
+
+	fmt.Print("Ingrese su edad: ")
+	fmt.Scanln(&edad)
+
+	fmt.Print("Ingrese el valor de pi: ")
+	fmt.Scanln(&pi)
+
+	fmt.Printf("Nombre: %s Edad: %d \nvalor de Pi: %f", nombre, edad, pi)
+	fmt.Println()
 }
