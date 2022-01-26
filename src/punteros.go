d1ef4b8d223e1b8c12a41f27e53b4920fbf4942c
CarolinaOM <caro13081991@gmail.com>
2022-01-26 15:10:24 +0000

    Punteros

---
+++ src/punteros.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	// a := 200
+	// b := &a
+	// *b++
+	// fmt.Println(a, b)
+	// res := fn()
+	// fmt.Println(fn() == fn())
+	v := 1
+	increment(&v)
+	fmt.Println(v)
+	increment(&v)
+	fmt.Println(v)
+}
+
+// func fn() *int {
+// 	v := 1
+// 	return &v
+// }
+
+func increment(p *int) int {
+	*p++
+	return *p
+}
