5e5e9c0a893b6bafde1c33660621103673d70272
CarolinaOM <caro13081991@gmail.com>
2022-01-27 19:53:33 +0000

    Agregando informacion en structs

--- src/structs.go
+++ src/structs.go
@@ -7,9 +7,18 @@ type User struct {
 	nombre, apellido string
 }
 
+func (this User) nombre_completo() string {
+	return this.nombre + " " + this.apellido
+}
+
+func (this *User) set_name(n string) {
+	this.nombre = n
+}
+
 func main() {
 	// fmt.Println(User{nombre: "Uriel", apellido: "Lopez"})
 	uriel := new(User)
 	uriel.nombre = "Uriel"
+	uriel.set_name("Marcos")
 	fmt.Println(uriel.nombre)
 }
